bde19ec0a9a52632a6b376355a84e7429b83b008
lanzv <vojtech.lanz@gmail.com>
2023-01-30 01:16:00 +0100

    add feature selection to eval pipeline

--- models/src/utils/eval.py
+++ models/src/utils/eval.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
@@ -8,11 +7,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.utils.fixes import loguniform
 import numpy as np
 import logging
-import sys
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
+from sklearn.feature_selection import SelectFromModel
+
 logging.getLogger().setLevel(logging.INFO)
 
 def list2string(segmented_chants):
@@ -22,17 +22,20 @@ def list2string(segmented_chants):
     return string_segmentations
 
 def bacor_eval(train_segmented_chants, train_modes,
-               test_segmented_chants, test_modes, seed = 0):
+               test_segmented_chants, test_modes, seed = 0, max_features = 100):
     model = _BacorModel()
     # set seed
     np.random.seed(seed)
-    # Prepare data
+
+    # prepare data
     train_data = list2string(train_segmented_chants)
     test_data = list2string(test_segmented_chants)
+
     # train
     train_pred, test_pred = model.run(X_train = train_data, y_train = train_modes,
                                        X_test = test_data, y_test = test_modes)
-    logging.info("The SVC model was trained with {} training data and {} testing data.".format(len(train_modes), len(test_modes)))
+    logging.info("The SVC model was trained with {} training data and {} testing data."
+        .format(len(train_modes), len(test_modes)))
 
     # evaluate
     scores = model.evaluate(train_pred, train_modes, test_pred, test_modes)
@@ -46,7 +49,13 @@ def bacor_eval(train_segmented_chants, train_modes,
                 scores["test"]["recall"],
                 scores["test"]["f1"],
                 scores["test"]["accuracy"]))
-    return scores
+
+    # feature selection
+    important_melodies = model.feature_selection(train_data, max_features = max_features)
+    logging.info("Top selected melodies: {}"
+        .format(important_melodies))
+
+    return scores, important_melodies
 
 
 
@@ -98,6 +107,20 @@ class _BacorModel:
             recall = recall_score(test_gold, test_pred, average='weighted'),
         )
         return scores
+
+    def feature_selection(self, X_data, max_features = None):
+        selector = SelectFromModel(estimator=self.model.steps[1][1], prefit = True, max_features = max_features)
+        X_vectorized = self.model.steps[0][1].transform(X_data)
+        print(X_vectorized.shape)
+        X_new = selector.transform(X_vectorized)
+        print(X_new.shape)
+        chants = self.model.steps[0][1].inverse_transform(X_new)
+        melodies = set()
+        for chant in chants:
+             for melody in chant:
+                melodies.add(melody)
+        return list(melodies)
+
 
     # ------------------------- Training methods -------------------------
 
