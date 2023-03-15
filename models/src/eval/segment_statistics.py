69ec9793294f191dca40e41d64ed615239accf96
lanzv <vojtech.lanz@gmail.com>
2023-03-15 23:53:26 +0100

    plotting mode statistics and confusion matrices

--- models/src/eval/segment_statistics.py
+++ models/src/eval/segment_statistics.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+from pandas import DataFrame
 
 def get_vocabulary_size(segmentation: list) -> int:
     """
@@ -23,3 +24,130 @@ def get_average_segment_length(segmentation: list) -> float:
             all_segments += 1
             segment_length_sum += len(segment)
     return float(segment_length_sum)/float(all_segments)
+
+def show_mode_segment_statistics(segmentation, modes, mode_list = ["1", "2", "3", "4", "5", "6", "7", "8"]):
+    shared_dataframe = get_shared_segments_mode_dataframe(segmentation, modes, mode_list)
+    distinct_dataframe = get_distinct_segments_mode_dataframe(segmentation, modes, mode_list)
+    vocabulary_sizes_dataframe = get_vocab_segments_mode_dataframe(segmentation, modes, mode_list)
+    unique_dataframe = get_unique_segments_mode_dataframe(segmentation, modes, mode_list)
+    
+
+def get_shared_segments_mode_dataframe(segmentation: list, modes: list, mode_list = ["1", "2", "3", "4", "5", "6", "7", "8"]):
+    """
+    segmentation is a list of lists of segments
+    [["asda", "asdasd", "as", "ds"]]
+    """
+    # Dictionary of all unique segments that occure in the specific mode
+    mode_unique_segments = {}
+    for mode in mode_list:
+        mode_unique_segments[mode] = set()
+
+    # Collect all unique segments of all modes
+    for chant, mode in zip(segmentation, modes):
+        for segment in chant:
+            mode_unique_segments[mode].add(segment)
+    
+    # Create the final dataframe
+    index = mode_list.copy()
+    columns = mode_list.copy()
+    shared_segments = np.zeros((len(index), len(columns))) 
+    for i in range(len(mode_list)):
+        for j in range(i+1, len(mode_list)):
+            count = 0
+            for segment in mode_unique_segments[mode_list[i]]:
+                if segment in mode_unique_segments[mode_list[j]]:
+                    count += 1
+            shared_segments[j, i] = count
+            shared_segments[i, j] = count
+    df = DataFrame(shared_segments, index=index, columns=columns)
+
+    return df
+
+
+def get_distinct_segments_mode_dataframe(segmentation: list, modes: list, mode_list = ["1", "2", "3", "4", "5", "6", "7", "8"]):
+    """
+    segmentation is a list of lists of segments
+    [["asda", "asdasd", "as", "ds"]]
+    """
+    # Dictionary of all unique segments that occure in the specific mode
+    mode_unique_segments = {}
+    for mode in mode_list:
+        mode_unique_segments[mode] = set()
+
+    # Collect all unique segments of all modes
+    for chant, mode in zip(segmentation, modes):
+        for segment in chant:
+            mode_unique_segments[mode].add(segment)
+    
+    # Create the final dataframe
+    index = mode_list.copy()
+    columns = mode_list.copy()
+    distinct_segments = np.zeros((len(index), len(columns))) 
+    for i in range(len(mode_list)):
+        for j in range(i+1, len(mode_list)):
+            count = 0
+            for segment in mode_unique_segments[mode_list[i]]:
+                if segment in mode_unique_segments[mode_list[j]]:
+                    count += 1
+            count = len(mode_unique_segments[mode_list[i]]) + len(mode_unique_segments[mode_list[j]]) - 2*count
+            distinct_segments[j, i] = count
+            distinct_segments[i, j] = count
+    df = DataFrame(distinct_segments, index=index, columns=columns)
+    
+    return df
+
+
+
+def get_vocab_segments_mode_dataframe(segmentation: list, modes: list, mode_list = ["1", "2", "3", "4", "5", "6", "7", "8"]):
+    """
+    segmentation is a list of lists of segments
+    [["asda", "asdasd", "as", "ds"]]
+    """
+    # Dictionary of all unique segments that occure in the specific mode
+    mode_unique_segments = {}
+    for mode in mode_list:
+        mode_unique_segments[mode] = set()
+
+    # Collect all unique segments of all modes
+    for chant, mode in zip(segmentation, modes):
+        for segment in chant:
+            mode_unique_segments[mode].add(segment)
+    
+    # Create the final dataframe
+    index = mode_list.copy()
+    vocab_segments = np.zeros((len(index))) 
+    for i in range(len(mode_list)):
+        vocab_segments[i] = len(mode_unique_segments[mode_list[i]])
+    df = DataFrame(vocab_segments, index=index)
+    
+    return df
+
+def get_unique_segments_mode_dataframe(segmentation: list, modes: list, mode_list = ["1", "2", "3", "4", "5", "6", "7", "8"]):
+    """
+    segmentation is a list of lists of segments
+    [["asda", "asdasd", "as", "ds"]]
+    """
+    # Dictionary of all unique segments that occure in the specific mode
+    mode_unique_segments = {}
+    for mode in mode_list:
+        mode_unique_segments[mode] = set()
+
+    # Collect all unique segments of all modes
+    for chant, mode in zip(segmentation, modes):
+        for segment in chant:
+            mode_unique_segments[mode].add(segment)
+    
+    # Create the final dataframe
+    index = mode_list.copy()
+    unique_segments = np.zeros((len(index))) 
+    for i in range(len(mode_list)):
+        temp_vocab = mode_unique_segments[mode_list[i]].copy()
+        for j in range(len(mode_list)):
+            if not i == j:
+                for segment in temp_vocab:
+                    if segment in mode_unique_segments[mode_list[j]]:
+                        temp_vocab.remove(segment)
+        unique_segments[i] = len(temp_vocab)
+    df = DataFrame(unique_segments, index=index)
+    
+    return df
--- models/src/utils/plotters.py
+++ models/src/utils/plotters.py
@@ -3,6 +3,64 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 
 
+def plot_mode_segment_statistics(
+        shared_dataframe: DataFrame,
+        distinct_dataframe: DataFrame,
+        vocabulary_sizes_dataframe: DataFrame,
+        unique_dataframe: DataFrame):
+    
+    figure, axis = plt.subplots(1, 4)
+
+    # Same segments in mode pair
+    axis[0].matshow(shared_dataframe, interpolation ='nearest')
+    axis[0].set_xticks(np.arange(len(shared_dataframe.index)), minor=False)
+    axis[0].set_xticklabels(shared_dataframe.index, fontdict=None, minor=False)
+    axis[0].set_yticks(np.arange(len(shared_dataframe.columns)), minor=False)
+    axis[0].set_yticklabels(shared_dataframe.columns, fontdict=None, minor=False)
+    axis[0].xaxis.set_ticks_position('bottom')
+    for (i, j), z in np.ndenumerate(shared_dataframe):
+        axis[0].text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
+    axis[0].set_title("Shared Segments")
+
+
+    # Different segments in mode pair
+    axis[1].matshow(distinct_dataframe, interpolation ='nearest')
+    axis[1].set_xticks(np.arange(len(distinct_dataframe.index)), minor=False)
+    axis[1].set_xticklabels(distinct_dataframe.index, fontdict=None, minor=False)
+    axis[1].set_yticks(np.arange(len(distinct_dataframe.columns)), minor=False)
+    axis[1].set_yticklabels(distinct_dataframe.columns, fontdict=None, minor=False)
+    axis[1].xaxis.set_ticks_position('bottom')
+    for (i, j), z in np.ndenumerate(distinct_dataframe):
+        axis[1].text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
+    axis[1].set_title("Distinct Segments")
+
+
+    # Vocabulary Size in modes
+    axis[2].matshow(vocabulary_sizes_dataframe, interpolation ='nearest')
+    axis[2].set_xticks(np.arange(len(vocabulary_sizes_dataframe.index)), minor=False)
+    axis[2].set_xticklabels(vocabulary_sizes_dataframe.index, fontdict=None, minor=False)
+    axis[2].set_yticks(np.arange(len(vocabulary_sizes_dataframe.columns)), minor=False)
+    axis[2].set_yticklabels(vocabulary_sizes_dataframe.columns, fontdict=None, minor=False)
+    axis[2].xaxis.set_ticks_position('bottom')
+    for (i, j), z in np.ndenumerate(vocabulary_sizes_dataframe):
+        axis[2].text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
+    axis[2].set_title("Vocabulary Size")
+
+    # Unique Segments in modes
+    axis[3].matshow(unique_dataframe, interpolation ='nearest')
+    axis[3].set_xticks(np.arange(len(unique_dataframe.index)), minor=False)
+    axis[3].set_xticklabels(unique_dataframe.index, fontdict=None, minor=False)
+    axis[3].set_yticks(np.arange(len(unique_dataframe.columns)), minor=False)
+    axis[3].set_yticklabels(unique_dataframe.columns, fontdict=None, minor=False)
+    axis[3].xaxis.set_ticks_position('bottom')
+    for (i, j), z in np.ndenumerate(unique_dataframe):
+        axis[3].text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
+    axis[3].set_title("Unique Segments")
+
+    plt.subplots_adjust(left=0.01, bottom=0.01, right=0.99, top=0.99, wspace=0.2, hspace=0.2)
+    plt.show()
+
+
 def plot_melody_mode_frequencies(frequencies: DataFrame):
     f = plt.figure()
     f.set_figwidth(len(frequencies.columns)*2)
