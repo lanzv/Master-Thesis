f9557719ded23bf70da90c2217013e46506f0d2a
lanzv <vojtech.lanz@gmail.com>
2023-03-19 16:01:00 +0100

    mjwp score

--- models/src/eval/mjww_score.py
+++ models/src/eval/mjww_score.py
@@ -1,4 +1,4 @@
-from src.utils.loader import load_word_segmentations
+from src.utils.loader import load_word_segmentations, load_phrase_segmentations
 
 def mjww_score(segmented_chants):
     """
@@ -22,4 +22,30 @@ def mjww_score(segmented_chants):
                 correct_segments += 1
         total_segments += len(chant_segments)
 
-    return correct_segments/total_segments
+    return float(correct_segments)/float(total_segments)
+
+
+def mjwp_score(model):
+    """
+    Melody Justified With Phrases score
+
+    Frequency of melodies that end with end of any word.
+    """
+    total_segments = 0
+    correct_segments = 0
+    phrased_chants = load_phrase_segmentations()
+    segmented_chants = model.predict_segments([''.join(phrased_chant) for phrased_chant in phrased_chants])
+    for chant_segments, phrase_segmentation in zip(segmented_chants, phrased_chants):
+        phrase_indices = set()
+        i = 0
+        for phrase in phrase_segmentation:
+            i += len(phrase)
+            phrase_indices.add(i)
+        i = 0
+        for segment in chant_segments:
+            i += len(segment)
+            if i in phrase_indices:
+                correct_segments += 1
+        total_segments += len(phrase_segmentation)
+
+    return float(correct_segments)/float(total_segments)
