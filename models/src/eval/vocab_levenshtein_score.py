9e0f02a9d24d73da8d10c40e133751b15985d499
lanzv <vojtech.lanz@gmail.com>
2023-04-09 00:37:20 +0200

    levenhstein score switch max to min

--- models/src/eval/vocab_levenshtein_score.py
+++ models/src/eval/vocab_levenshtein_score.py
@@ -1,11 +1,12 @@
 from Levenshtein import distance
 import numpy as np
+import sys
 
 def vocab_levenshtein_score(segmented_chants):
     """
     First, get the vocabulary from segmented chants.
     Second, compute levenhstein distances of each pair of vocabulary segments.
-    The score compute as 1/N sum_{vocab_segment}(max(levenshtein distances with between vocab segment and all rest vocab segments))
+    The score compute as 1/N sum_{vocab_segment}(min(levenshtein distances with between vocab segment and all rest vocab segments))
 
     Parameters
     ----------
@@ -14,7 +15,7 @@ def vocab_levenshtein_score(segmented_chants):
     Returns
     -------
     levenshtein_score : float
-        1/N sum_{vocab_segment}(max(levenshtein distances with between vocab segment and all rest vocab segments))
+        1/N sum_{vocab_segment}(min(levenshtein distances with between vocab segment and all rest vocab segments))
     """
     vocab = set()
     for chant in segmented_chants:
@@ -23,9 +24,10 @@ def vocab_levenshtein_score(segmented_chants):
     vocab = list(vocab)
     distance_matrix = np.zeros((len(vocab), len(vocab)))
     for i in range(len(vocab)):
+        distance_matrix[i, i] = sys.maxsize
         for j in range(i+1, len(vocab)):
             i_j_distance = distance(vocab[i], vocab[j])
             distance_matrix[i, j] = i_j_distance
             distance_matrix[j ,i] = i_j_distance
-    levenshtein_score = np.average(np.amax(distance_matrix, axis=0))
+    levenshtein_score = np.average(np.amin(distance_matrix, axis=0))
     return levenshtein_score
