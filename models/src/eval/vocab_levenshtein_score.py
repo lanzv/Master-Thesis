48fb9ea78aff697cb3fb5c91f2d23328a3d4a911
lanzv <vojtech.lanz@gmail.com>
2023-04-15 18:58:15 +0200

    responsories unigram model, levenhtein score via median of same sizes

--- models/src/eval/vocab_levenshtein_score.py
+++ models/src/eval/vocab_levenshtein_score.py
@@ -1,12 +1,13 @@
 from Levenshtein import distance
 import numpy as np
-import sys
+import logging
 
 def vocab_levenshtein_score(segmented_chants):
     """
     First, get the vocabulary from segmented chants.
     Second, compute levenhstein distances of each pair of vocabulary segments.
-    The score compute as 1/N sum_{vocab_segment}(min(levenshtein distances with between vocab segment and all rest vocab segments))
+    The score compute as 1/N sum_{vocab_segment}(median(levenshtein distances between vocab 
+                                    segment and all other vocab segments with same size))
 
     Parameters
     ----------
@@ -15,19 +16,35 @@ def vocab_levenshtein_score(segmented_chants):
     Returns
     -------
     levenshtein_score : float
-        1/N sum_{vocab_segment}(min(levenshtein distances with between vocab segment and all rest vocab segments))
+        1/N sum_{vocab_segment}(median(levenshtein distances between vocab
+                                    segment and all other vocab segments with same size))
     """
-    vocab = set()
+    median_sum = 0
+    segment_count = 0
+    vocab_sizes = {}
     for chant in segmented_chants:
         for segment in chant:
-            vocab.add(segment)
-    vocab = list(vocab)
-    distance_matrix = np.zeros((len(vocab), len(vocab)))
-    for i in range(len(vocab)):
-        distance_matrix[i, i] = sys.maxsize
-        for j in range(i+1, len(vocab)):
-            i_j_distance = distance(vocab[i], vocab[j])
-            distance_matrix[i, j] = i_j_distance
-            distance_matrix[j ,i] = i_j_distance
-    levenshtein_score = np.average(np.amin(distance_matrix, axis=0))
+            if len(segment) in vocab_sizes:
+                vocab_sizes[len(segment)].add(segment)
+            else:
+                vocab_sizes[len(segment)] = {segment}
+    for size in vocab_sizes:
+        vocab = list(vocab_sizes[size])
+        if len(vocab) <= 1:
+            logging.warn("There is a ignored {} segments with size {} when computing levenhstein score.".format(len(vocab), size))
+            continue
+        segment_count += len(vocab)
+        distance_matrix = np.zeros((len(vocab), len(vocab)))
+        for i in range(len(vocab)):
+            for j in range(i+1, len(vocab)):
+                i_j_distance = distance(vocab[i], vocab[j])
+                distance_matrix[i, j] = i_j_distance
+                distance_matrix[j ,i] = i_j_distance
+        median_sum += np.sum(
+            np.nanmedian(
+                np.where(distance_matrix == 0.0, np.nan, distance_matrix)
+                , axis=0
+                )
+            )
+    levenshtein_score = median_sum/segment_count
     return levenshtein_score
