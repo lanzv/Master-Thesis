f8631d08b139c9f6a33796f736911ccb262003b5
lanzv <vojtech.lanz@gmail.com>
2023-04-24 11:01:50 +0200

    normalization of levenhetein

--- models/src/eval/vocab_levenshtein_score.py
+++ models/src/eval/vocab_levenshtein_score.py
@@ -6,8 +6,8 @@ def vocab_levenshtein_score(segmented_chants):
     """
     First, get the vocabulary from segmented chants.
     Second, compute levenhstein distances of each pair of vocabulary segments.
-    The score compute as 1/N sum_{vocab_segment}(median(levenshtein distances between vocab 
-                                    segment and all other vocab segments with same size))
+    The score is computed as 1/N sum_{vocab_segment}(normalization_segment_length
+    *median(levenshtein distances between vocab  segment and all other vocab segments with same size))
 
     Parameters
     ----------
@@ -16,8 +16,8 @@ def vocab_levenshtein_score(segmented_chants):
     Returns
     -------
     levenshtein_score : float
-        1/N sum_{vocab_segment}(median(levenshtein distances between vocab
-                                    segment and all other vocab segments with same size))
+        1/N sum_{vocab_segment}(normalization_segment_length*median(levenshtein 
+            distances between vocab  segment and all other vocab segments with same size))
     """
     median_sum = 0
     segment_count = 0
@@ -40,7 +40,8 @@ def vocab_levenshtein_score(segmented_chants):
                 i_j_distance = distance(vocab[i], vocab[j])
                 distance_matrix[i, j] = i_j_distance
                 distance_matrix[j ,i] = i_j_distance
-        median_sum += np.sum(
+        normalizer = 1.0/(size)
+        median_sum += normalizer * np.sum(
             np.nanmedian(
                 np.where(distance_matrix == 0.0, np.nan, distance_matrix)
                 , axis=0
